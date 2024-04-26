66c564284cdf08bf9b67f216ccf1333c9b1f72aa
Pim Mulder <pim.heerde@gmail.com>
2024-04-26 17:08:57 +0200

    Make cluster compatible

--- src/simulations/confidentiality/run_simulation.py
+++ src/simulations/confidentiality/run_simulation.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Keep at top, so cluster knows which directory to work in
+PROJECT_DIRECTORY = os.path.abspath(os.path.join(os.getcwd(), "./"))
+sys.path.append(PROJECT_DIRECTORY)
+
 import argparse
 import os
 import pickle
