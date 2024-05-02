4c0e8c31f4fd59eb3ba8b0456905f1dcd3928723
Pim Mulder <pim.heerde@gmail.com>
2024-05-02 12:27:32 +0200

    Print if cuda is loaded correctly

--- src/simulations/performance/run_simulation.py
+++ src/simulations/performance/run_simulation.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from src import training
+
 # Keep at top, so cluster knows which directory to work in
 PROJECT_DIRECTORY = os.path.abspath(os.path.join(os.getcwd(), "./"))
 sys.path.append(PROJECT_DIRECTORY)
@@ -193,6 +195,7 @@ def run_simulation(
 
 
 def main() -> None:
+	log(INFO, training.DEVICE)
 	args = parser.parse_args()
 
 	client_resources = {
