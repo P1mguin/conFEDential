df8fdc1fdba4982b6eef65e7e3d9678bf1108c9e
Pim Mulder <pim.heerde@gmail.com>
2024-05-13 22:58:10 +0200

    Import learning methods with alternative names

--- src/training/learning_methods/__init__.py
+++ src/training/learning_methods/__init__.py
@@ -5,3 +5,19 @@ from .FedNL import FedNL
 from .RMSprop import RMSprop
 from .SCAFFOLD import SCAFFOLD
 from .Strategy import Strategy
+
+FedNag = FedNAG
+fednag = FedNAG
+FEDNAG = FedNag
+FEDADAM = FedAdam
+fedadam = FedAdam
+FedADAM = FedAdam
+FEDAVG = FedAvg
+fedavg = FedAvg
+FedAVG = FedAvg
+FEDNL = FedNL
+fednl = FedNL
+FedNl = FedNL
+RMSProp = RMSprop
+rmsprop = RMSprop
+RMSPROP = RMSProp
