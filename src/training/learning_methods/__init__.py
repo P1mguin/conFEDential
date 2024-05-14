022da9f5f015c686ca94e94d656f2479eb746df8
Pim Mulder <pim.heerde@gmail.com>
2024-05-14 15:28:35 +0200

    Removed SCAFFOLD

--- src/training/learning_methods/__init__.py
+++ src/training/learning_methods/__init__.py
@@ -3,7 +3,6 @@ from .FedAvg import FedAvg
 from .FedNAG import FedNAG
 from .FedNL import FedNL
 from .RMSprop import RMSprop
-from .SCAFFOLD import SCAFFOLD
 from .Strategy import Strategy
 
 FedNag = FedNAG
