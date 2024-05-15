2281d5e623bab04ac4e6bb55a6a40044427320cd
Pim Mulder <pim.heerde@gmail.com>
2024-05-15 10:17:15 +0200

    Remove ImageNet

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,5 @@
 from .CIFAR10 import CIFAR10
 from .CIFAR100 import CIFAR100
-from .ImageNet import ImageNet
 from .MNIST import MNIST
 from .Purchase import Purchase
 from .Texas import Texas
@@ -8,14 +7,12 @@ from .Texas import Texas
 # Also load as lowercase and uppercase names
 cifar10 = CIFAR10
 cifar100 = CIFAR100
-imagenet = ImageNet
 mnist = MNIST
 purchase = Purchase
 texas = Texas
 
 CIFAR10 = CIFAR10
 CIFAR100 = CIFAR100
-IMAGENET = ImageNet
 MNIST = MNIST
 PURCHASE = Purchase
 TEXAS = Texas
