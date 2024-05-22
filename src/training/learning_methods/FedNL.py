2fcf1c9f3175c67b7589dccc1da21e9d00a6fcc0
Pim Mulder <pim.heerde@gmail.com>
2024-05-22 13:27:30 +0200

    Revert "Testing reshaping matrix"
    This reverts commit 85986dce2032ed5c1280ef1f0fa169ea75afbd8b.

--- src/training/learning_methods/FedNL.py
+++ src/training/learning_methods/FedNL.py
@@ -11,7 +11,6 @@ from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
 from src import training, utils
-from src.experiment import Config
 from src.training.learning_methods.Strategy import Strategy
 
 
@@ -21,51 +20,70 @@ class NewtonOptimizer(Optimizer):
 		defaults = dict()
 		super(NewtonOptimizer, self).__init__(params, defaults)
 
-	def step(self, closure: Callable[[torch.Tensor], torch.Tensor] | None = None) -> Optional[float]:
+	def step(self, closure: Callable[[Tuple[torch.Tensor]], torch.Tensor] | None = None) -> Optional[float]:
 		# The closure function is required to compute the Hessian, if it is not there raise an error
 		if closure is None:
 			raise RuntimeError('closure function must be provided for NewtonOptimizer')
 
-		model_parameters = [value for param_group in self.param_groups for value in param_group["params"]]
+		model_parameters = tuple(value for param_group in self.param_groups for value in param_group["params"])
 
-		# Flatten model
-		parameter_shapes = [torch.tensor(layer.shape) for layer in model_parameters]
-		flat_parameter_shapes = torch.stack([torch.prod(shape) for shape in parameter_shapes])
-		cutoff_points = [0, *torch.cumsum(flat_parameter_shapes, dim=0)]
-		cutoff_points = list(zip(cutoff_points, cutoff_points[1:]))
-		model_parameters = torch.concatenate([value.flatten() for value in model_parameters])
+		# The gradients are directly computed from the closure function
+		loss = closure(*model_parameters)
+		gradients = torch.autograd.grad(loss, model_parameters)
 
-		# Compute the gradient and hessian
-		loss = closure(model_parameters)
-		gradient = torch.autograd.grad(loss, model_parameters)[0]
-		hessian = torch.func.hessian(closure)(model_parameters) + torch.eye(len(gradient)) * 1e-8
-		inv_hessian = torch.linalg.inv(hessian)
-		update = inv_hessian @ gradient
-
-		# Reshape gradients, hessian and update to its desired shape
+		# Ensure the gradients are at least 2D with shape (out_feature, in_feature), the tuple in the list is
+		# (gradient, is_expanded). We need the boolean to correct the expansion later
 		gradients = [
-			gradient[start:end].reshape(*parameter_shape)
-			for (start, end), parameter_shape in zip(cutoff_points, parameter_shapes)
-		]
-		hessians = [
-			hessian[start:end, start:end].reshape(*parameter_shape, *parameter_shape)
-			for (start, end), parameter_shape in zip(cutoff_points, parameter_shapes)
-		]
-		updates = [
-			update[start:end].reshape(*parameter_shape)
-			for (start, end), parameter_shape in zip(cutoff_points, parameter_shapes)
+			(gradient.unsqueeze(-1), True) if gradient.ndim == 1 else (gradient, False)
+			for gradient in gradients
 		]
 
-		i = 0
-		for param_group in self.param_groups:
-			for layer in param_group["params"]:
-				gradient = gradients[i]
-				hessian = hessians[i]
-				update = updates[i]
-				self.state[layer]["gradients"] = gradient
-				self.state[layer]["hessian"] = hessian
-				layer.data.sub_(update)
-				i += 1
+		# The hessian is computed using torch func
+		hessians = torch.func.hessian(closure, argnums=tuple(range(len(model_parameters))))(*model_parameters)
+		hessians = (hessian[i] for i, hessian in enumerate(hessians))
+
+		# Expand the hessians similarly, do not keep track of the boolean as that is in the gradients
+		hessians = (
+			hessian.unsqueeze(-2).unsqueeze(-1) if is_expanded else hessian
+			for (_, is_expanded), hessian in zip(gradients, hessians)
+		)
+
+		# Reshape the matrix to be square, the hessian at index i, j is hessian[i, :, j]
+		hessians = (torch.stack([hessian[i, :, i] for i in range(hessian.size(0))]) for hessian in hessians)
+
+		# Use a generator to limit memory usage
+		def compute_inverse_hessian(hessian):
+			for out_feature_hessian in hessian:
+				try:
+					inv_hessian = torch.linalg.inv(out_feature_hessian)
+					inv_hessian[inv_hessian.isnan()] = 0.
+				except torch.linalg.LinAlgError:
+					inv_hessian = torch.ones_like(out_feature_hessian)
+				yield inv_hessian
+
+		# Compute the update per layer
+		for i in range(len(gradients)):
+			gradient, is_expanded = gradients[i]
+			hessian = next(hessians)
+			inverse_hessian = compute_inverse_hessian(hessian)
+			update = torch.stack([
+				out_feature_inverse_hessian @ out_feature_gradient
+				for out_feature_inverse_hessian, out_feature_gradient in zip(inverse_hessian, gradient)
+			])
+
+			# Correct the expansion
+			if is_expanded:
+				hessian = hessian.view(-1)
+				gradient = gradient.view(-1)
+				update = update.view(-1)
+
+			# Set the state with the gradient and hessian, and update the model parameters
+			self.state[model_parameters[i]]["gradients"] = gradient.detach()
+			self.state[model_parameters[i]]["hessian"] = hessian.detach()
+			model_parameters[i].data.sub_(update)
+
+		# Return the loss
+		return loss.item()
 
 
 class FedNL(Strategy):
@@ -91,25 +109,14 @@ class FedNL(Strategy):
 		optimizer = simulation.get_optimizer(net.parameters())
 		local_rounds = simulation.local_rounds
 
-		parameter_shapes = [layer.shape for layer in parameters]
-		flat_parameter_shapes = [np.prod(shape) for shape in parameter_shapes]
-		cutoff_points = [0, *np.cumsum(flat_parameter_shapes)]
-		cutoff_points = list(zip(cutoff_points, cutoff_points[1:]))
-
 		# Do local rounds and epochs
 		for _ in range(local_rounds):
 			for features, labels in train_loader:
 				features, labels = features.to(training.DEVICE), labels.to(training.DEVICE)
 
-				def closure(weights):
-					# Reshape the weights to the correct shape
-					weights = [
-						weights[start:end].reshape(parameter_shape)
-						for (start, end), parameter_shape in zip(cutoff_points, parameter_shapes)
-					]
-					net_parameters = {
-						name: value for name, value in zip(net.state_dict().keys(), weights)
-					}
+				def get_gradient(*weights):
+					# Query the model as if the parameters had been set
+					net_parameters = {name: value for name, value in zip(net.state_dict().keys(), weights)}
 					prediction = torch.func.functional_call(net, net_parameters, features)
 
 					# Set the parameters as the state dict of the model
@@ -117,11 +124,11 @@ class FedNL(Strategy):
 					return loss
 
 				optimizer.zero_grad()
-				optimizer.step(closure)
+				optimizer.step(get_gradient)
 
 		# Take the gradients and hessian from the optimizer state and transmit the results
 		gradients = [value["gradients"] for value in optimizer.state_dict()["state"].values()]
-		hessian = [value["hessian"].detach().numpy() for value in optimizer.state_dict()["state"].values()]
+		hessian = [value["hessian"] for value in optimizer.state_dict()["state"].values()]
 		data_size = len(train_loader.dataset)
 		return gradients, data_size, {"hessian": hessian}
 
@@ -146,73 +153,42 @@ class FedNL(Strategy):
 		hessian_results = (fit_res.metrics["hessian"] for _, fit_res in results)
 		hessians = utils.common.compute_weighted_average(hessian_results, counts)
 
-		parameter_shapes = [layer.shape for layer in gradients]
-		flat_parameter_shapes = [np.prod(shape) for shape in parameter_shapes]
-		cutoff_points = [0, *np.cumsum(flat_parameter_shapes)]
-		cutoff_points = list(zip(cutoff_points, cutoff_points[1:]))
-
-		gradients = np.concatenate([value.flatten() for value in gradients])
-		hessian_template = np.zeros((len(gradients), len(gradients)))
-		for i, ((start, end), hessian) in enumerate(zip(cutoff_points, hessians)):
-			size = end - start
-			hessian = hessian.reshape(size, size)
-			hessian_template[start:end, start:end] = hessian
-		hessian = hessian_template
-		inv_hessian = np.linalg.inv(hessian)
-		update = inv_hessian @ gradients
-		updates = (
-			update[start:end].reshape(*parameter_shape)
-			for (start, end), parameter_shape in zip(cutoff_points, parameter_shapes)
-		)
-		self.current_weights = ndarrays_to_parameters([
-			layer - update for layer, update in zip(self.current_weights, updates)
-		])
-		return self.current_weights, {}
-
-if __name__ == '__main__':
-	path = "examples/mnist/logistic_regression/fed_nl.yaml"
-	config = Config.from_yaml_file(path)
-	data_loader = config.simulation.train_loaders[0]
-	model = config.simulation.model
-	initial_parameters = training.get_weights(model)
-
-	# Test the initial parameters performance
-	loss, accuracy, _ = Strategy.test(initial_parameters, data_loader, config.simulation)
-	print(f"Initial loss: {loss}, accuracy: {accuracy}")
-
-	current_weights = initial_parameters
-	for _ in range(10):
-		gradients, _, metrics = config.simulation.learning_method.train(
-			parameters=current_weights,
-			train_loader=data_loader,
-			simulation=config.simulation,
-			metrics={}
-		)
-
-		gradients = [np.array(gradient) for gradient in gradients]
-		hessians = [np.array(hessian) for hessian in metrics["hessian"]]
-
-		parameter_shapes = [layer.shape for layer in gradients]
-		flat_parameter_shapes = [np.prod(shape) for shape in parameter_shapes]
-		cutoff_points = [0, *np.cumsum(flat_parameter_shapes)]
-		cutoff_points = list(zip(cutoff_points, cutoff_points[1:]))
-
-		gradients = np.concatenate([value.flatten() for value in gradients])
-		hessian_template = np.zeros((len(gradients), len(gradients)))
-		for i, ((start, end), hessian) in enumerate(zip(cutoff_points, hessians)):
-			size = end - start
-			hessian = hessian.reshape(size, size)
-			hessian_template[start:end, start:end] = hessian
-		hessian = hessian_template
-		inv_hessian = np.linalg.inv(hessian)
-		update = inv_hessian @ gradients
-		updates = (
-			update[start:end].reshape(*parameter_shape)
-			for (start, end), parameter_shape in zip(cutoff_points, parameter_shapes)
-		)
-		current_weights = [
-			layer - update for layer, update in zip(current_weights, updates)
+		gradients = [
+			(np.expand_dims(gradient, axis=-1), True) if gradient.ndim == 1 else (gradient, False)
+			for gradient in gradients
 		]
+		hessians = (
+			np.expand_dims(hessian, axis=(-2, -1)) if is_expanded else hessian
+			for (_, is_expanded), hessian in zip(gradients, hessians)
+		)
 
-		loss, accuracy, _ = Strategy.test(current_weights, data_loader, config.simulation)
-		print(f"Updated loss: {loss}, accuracy: {accuracy}")
+		def compute_inverse_hessian(hessian):
+			for out_feature_hessian in hessian:
+				try:
+					inv_hessian = np.linalg.inv(out_feature_hessian)
+				except np.linalg.LinAlgError:
+					inv_hessian = np.ones_like(out_feature_hessian)
+				yield inv_hessian
+
+		# Per layer calculate the model update
+		current_weights = parameters_to_ndarrays(self.current_weights)
+		for i in range(len(gradients)):
+			gradient, is_expanded = gradients[i]
+			hessian = next(hessians)
+			inverse_hessian = compute_inverse_hessian(hessian)
+			del hessian
+
+			update = np.stack([
+				out_feature_inverse_hessian @ out_feature_gradient
+				for out_feature_inverse_hessian, out_feature_gradient in zip(inverse_hessian, gradient)
+			])
+			del inverse_hessian, gradient
+
+			# Correct the expansion
+			if is_expanded:
+				update = update.reshape(-1)
+
+			current_weights[i] -= update
+
+		self.current_weights = ndarrays_to_parameters(current_weights)
+		return self.current_weights, {}
