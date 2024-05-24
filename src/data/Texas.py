d3d552b6c96a125680ae8017200d24624e9c924c
Pim Mulder <pim.heerde@gmail.com>
2024-05-24 16:03:17 +0200

    Fix indices of split_label_and_features

--- src/data/Texas.py
+++ src/data/Texas.py
@@ -22,8 +22,8 @@ class Texas(Dataset):
 		def split_label_and_features(entry):
 			entry = np.array(list(entry.values()))
 			return {
-				"label": entry[0],
-				"features": entry[1:]
+				"label": entry[-1],
+				"features": entry[:-1]
 			}
 
 		train_dataset = train_dataset.map(split_label_and_features)
