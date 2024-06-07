480a04fe0043ade8f71abe834e5cf2c4e30f7b01
Pim Mulder <pim.heerde@gmail.com>
2024-06-07 09:33:04 +0200

    Order jobs alphabetically

--- hpc_runs/run_experiments_separately.sh
+++ hpc_runs/run_experiments_separately.sh
@@ -19,7 +19,7 @@ wandb sync --clean
 # Submit the sbatch jobs
 sbatch --nodes=1 hpc_runs/cifar10/resnet18.sbatch
 sbatch --nodes=1 hpc_runs/cifar100/resnet34.sbatch
-sbatch --nodes=1 hpc_runs/purchase/logistic_regression.sbatch
 sbatch --nodes=1 hpc_runs/purchase/fcn.sbatch
-sbatch --nodes=1 hpc_runs/texas/logistic_regression.sbatch
+sbatch --nodes=1 hpc_runs/purchase/logistic_regression.sbatch
 sbatch --nodes=1 hpc_runs/texas/fcn.sbatch
+sbatch --nodes=1 hpc_runs/texas/logistic_regression.sbatch
