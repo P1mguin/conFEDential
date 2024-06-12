6ec9a7a3d83ff64592bd4bda0d972dbc93788007
Pim Mulder <pim.heerde@gmail.com>
2024-06-12 12:28:58 +0200

    Put component construction in a separate file to set up origin net code reusage

--- src/attacks/MembershipNet.py
+++ src/attacks/MembershipNet.py
@@ -1,11 +1,9 @@
-import copy
-import operator
-from functools import reduce
+from typing import Dict, List, Sequence
 
 import torch
 import torch.nn as nn
 
-from src import utils
+import src.attacks.component_constructor as component_constructor
 
 
 class MembershipNet(nn.Module):
@@ -55,68 +53,22 @@ class MembershipNet(nn.Module):
 		prediction = self.encoder_component(encoder_input).squeeze(1)
 		return prediction
 
-	def _initialize_components(self, gradient_shapes, activation_shapes, metrics_shapes, label_shape):
-		self._initialize_label_component(label_shape)
-		self._initialize_loss_component()
-		self._initialize_activation_component(activation_shapes)
-		self._initialize_gradient_component(gradient_shapes)
-		self._initialize_metric_component(metrics_shapes)
-		self._initialize_encoder_component(activation_shapes, gradient_shapes, metrics_shapes)
-
-	def _initialize_label_component(self, label_shape):
-		layers = self._get_fcn_layers(label_shape[0])
-		label_component = nn.Sequential(*layers)
-		self.label_component = label_component
-
-	def _initialize_loss_component(self):
-		layers = self._get_fcn_layers(1)
-		layers.append(nn.Flatten(start_dim=1))
-		loss_component = nn.Sequential(*layers)
-		self.loss_component = loss_component
-
-	def _initialize_activation_component(self, activation_shapes):
-		# Flatten the activation components along the second axis, the first is the global rounds. They go into an
-		# FCN so need flattening
-		activation_components = [[nn.Flatten(start_dim=2)] for _ in activation_shapes]
-		activation_shapes = [shape[1:] for shape in activation_shapes]
-		activation_shapes = [reduce(operator.mul, shape, 1) for shape in activation_shapes]
-
-		for activation_shape, activation_component in zip(activation_shapes, activation_components):
-			activation_component.extend(self._get_fcn_layers(activation_shape))
-
-		self.activation_components = nn.ModuleList([nn.Sequential(*component) for component in activation_components])
-
-	def _initialize_gradient_component(self, gradient_shapes):
-		gradient_components = [self._get_convolution_layers(gradient_shape) for gradient_shape in gradient_shapes]
-
-		out_channels = next(
-			layer.out_channels for layer in reversed(gradient_components[0]) if hasattr(layer, "out_channels")
+	def _initialize_components(
+			self,
+			gradient_shapes: List[Sequence[int]],
+			activation_shapes: List[Sequence[int]],
+			metrics_shapes: Dict[str, List[Sequence[int]]],
+			label_shape: Sequence[int]
+	):
+		self.label_component = component_constructor.construct_label_component(self.config, label_shape)
+		self.loss_component = component_constructor.construct_loss_component(self.config)
+		self.activation_components = component_constructor.construct_activation_component(
+			self.config,
+			activation_shapes
 		)
-
-		# Add the fcn components to the convolutional components
-		for gradient_component in gradient_components:
-			gradient_component.append(nn.Flatten(start_dim=-3))
-			gradient_component.extend(self._get_fcn_layers(out_channels))
-
-		self.gradient_components = nn.ModuleList([nn.Sequential(*component) for component in gradient_components])
-
-	def _initialize_metric_component(self, metrics_shapes):
-		# Get the metrics from the config and then copy with equal shape as the
-		self.metric_components = {}
-		for key, metric_shapes in metrics_shapes.items():
-			metric_components = [self._get_convolution_layers(metric_shape) for metric_shape in metric_shapes]
-
-			out_channels = next(
-				layer.out_channels for layer in reversed(metric_components[0]) if hasattr(layer, "out_channels")
-			)
-
-			# Add the fcn components to the convolutional components
-			for metric_component in metric_components:
-				metric_component.append(nn.Flatten(start_dim=-3))
-				metric_component.extend(self._get_fcn_layers(out_channels))
-
-			self.metric_components[key] = nn.ModuleList([nn.Sequential(*component) for component in metric_components])
-		self.metric_components = nn.ModuleDict(self.metric_components)
+		self.gradient_components = component_constructor.construct_gradient_component(self.config, gradient_shapes)
+		self.metric_components = component_constructor.construct_metric_component(self.config, metrics_shapes)
+		self._initialize_encoder_component(activation_shapes, gradient_shapes, metrics_shapes)
 
 	def _initialize_encoder_component(self, activation_shapes, gradient_shapes, metrics_shapes):
 		# Keep track of the size that the encoder needs to take
@@ -150,61 +102,4 @@ class MembershipNet(nn.Module):
 				sum(metric_shapes[-4] for metric_shapes in metrics_shapes[key]) for key in metrics_shapes.keys()
 			) * metric_output_size * round_multiplier
 
-		# Construct the encoder component knowing the size of the input
-		layers = []
-		in_features_set = False
-		raw_encoder_copy = copy.deepcopy(self.config.attack.attack_simulation.model_architecture.encoder)
-		for layer in raw_encoder_copy:
-			layer_type = getattr(nn, layer["type"])
-			parameters = {key: value for key, value in list(layer.items())[1:]}
-
-			# The first linear layer of the attack has the size of the concatenated output of all components
-			if not in_features_set and layer_type is nn.Linear:
-				in_features_set = True
-				parameters = {**parameters, "in_features": encoder_size}
-
-			layer = layer_type(**parameters)
-			layers.append(layer)
-		encoder_component = nn.Sequential(*layers)
-		self.encoder_component = encoder_component
-
-	def _get_fcn_layers(self, input_size: int):
-		layers = []
-		raw_fcn_copy = copy.deepcopy(self.config.attack.attack_simulation.model_architecture.fcn)
-		dynamic_parameters_set = False
-		for layer in raw_fcn_copy:
-			layer_type = getattr(nn, layer["type"])
-			parameters = {key: value for key, value in list(layer.items())[1:]}
-
-			# The first linear layer of the attack has as many inputs as there are output classes
-			if not dynamic_parameters_set and layer_type is nn.Linear:
-				dynamic_parameters_set = True
-				parameters = {**parameters, "in_features": input_size}
-
-			layer = layer_type(**parameters)
-			layers.append(layer)
-		return layers
-
-	def _get_convolution_layers(self, input_shape):
-		layers = []
-		raw_cnn_copy = copy.deepcopy(self.config.attack.attack_simulation.model_architecture.gradient)
-		dynamic_parameters_set = False
-		for layer in raw_cnn_copy:
-			# Get the static parameters
-			if hasattr(nn, layer["type"]):
-				module = nn
-			else:
-				module = utils
-
-			layer_type = getattr(module, layer["type"])
-			parameters = {key: value for key, value in list(layer.items())[1:]}
-
-			# The kernel of the first convolution layer is as wide as the width/breadth of the input shape
-			if not dynamic_parameters_set and layer_type is nn.Conv2d:
-				parameters["in_channels"] = input_shape[-3]
-				parameters["kernel_size"] = input_shape[-2:]
-				dynamic_parameters_set = True
-
-			layer = layer_type(**parameters)
-			layers.append(layer)
-		return layers
+		self.encoder_component = component_constructor.construct_encoder_component(self.config, encoder_size, 1)
