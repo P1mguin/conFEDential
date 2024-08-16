0f69e05e1310aa02cfccccbd8064cef32ce14c51
Pim Mulder <pim.heerde@gmail.com>
2024-08-16 20:52:59 +0200

    Repeat simulation that did not work

--- hpc_runs/run_experiments_per_learning_method.sh
+++ hpc_runs/run_experiments_per_learning_method.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 scripts=(
-    "hpc_runs/texas/logistic_regression/fed_adam.sbatch"            # Not run simulation yet
+#    "hpc_runs/texas/logistic_regression/fed_adam.sbatch"            # Not run simulation yet
 #    "hpc_runs/texas/logistic_regression/fed_avg.sbatch"            # Only attack simulation left
 #    "hpc_runs/texas/logistic_regression/fed_nag.sbatch"            # Only attack simulation left
 #    "hpc_runs/texas/logistic_regression/fed_nl.sbatch"             # Only attack simulation left
-    "hpc_runs/texas/fcn/fed_adam.sbatch"                            # Not run simulation yet
+#    "hpc_runs/texas/fcn/fed_adam.sbatch"                            # Not run simulation yet
 #    "hpc_runs/texas/fcn/fed_avg.sbatch"                            # Only attack simulation left
 #    "hpc_runs/texas/fcn/fed_nag.sbatch"                            # Only attack simulation left
-    "hpc_runs/purchase/logistic_regression/fed_adam.sbatch"         # Not run simulation yet
+#    "hpc_runs/purchase/logistic_regression/fed_adam.sbatch"         # Not run simulation yet
 #    "hpc_runs/purchase/logistic_regression/fed_avg.sbatch"         # Only attack simulation left
 #    "hpc_runs/purchase/logistic_regression/fed_nag.sbatch"         # Only attack simulation left
 #    "hpc_runs/purchase/logistic_regression/fed_nl.sbatch"          # Only attack simulation left
-    "hpc_runs/purchase/fcn/fed_adam.sbatch"                         # Not run simulation yet
+#    "hpc_runs/purchase/fcn/fed_adam.sbatch"                         # Not run simulation yet
 #    "hpc_runs/purchase/fcn/fed_avg.sbatch"                         # Only attack simulation left
     "hpc_runs/purchase/fcn/fed_nag.sbatch"                          # Not run simulation yet
 #    "hpc_runs/cifar10/resnet18/fed_adam.sbatch"                    # Only attack simulation left
